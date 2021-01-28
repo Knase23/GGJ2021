edf364620d3ab18ef15b04fd7fba8da54721ca2f
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-01-28 12:40:20 +0100

    Added AI script on Giant Prefab

--- Assets/Scripts/Gameplay/AI/TriggerAiTalk.cs
+++ Assets/Scripts/Gameplay/AI/TriggerAiTalk.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 using Game.Gameplay.AI;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class TriggerAiTalk : MonoBehaviour
 {
     public AIDialogue talker;
+    private CircleCollider2D Collider2D;
+    
     private void Start()
     {
-        talker = GetComponent<AIDialogue>();
+        talker ??= GetComponent<AIDialogue>();
+        Collider2D = GetComponent<CircleCollider2D>();
+        Collider2D.radius = talker.talkRange;
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Something Entered the Trigger, something with tag: "+ other.tag);
