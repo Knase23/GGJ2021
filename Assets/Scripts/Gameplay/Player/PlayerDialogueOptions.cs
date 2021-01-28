4b3862fb45d52c51efce5d3b0881c45492444b07
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-01-28 10:51:01 +0100

    Improved DialogueSystem

---
+++ Assets/Scripts/Gameplay/Player/PlayerDialogueOptions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay.Player
+{
+    public class PlayerDialogueOptions : MonoBehaviour, IHearing, ITalker
+    {
+        public List<Hierogplyph> knownGlyphs = new List<Hierogplyph>();
+
+        public float talkRange = 3;
+        public Hierogplyph latestTalk;
+
+        private void OnEnable()
+        {
+            DialogueSystem.AddIHearingToList(this);
+        }
+
+        private void OnDisable()
+        {
+            DialogueSystem.RemoveIHearingToList(this);
+        }
+
+        private void LearnNewGlyph(Hierogplyph hieroglyphic)
+        {
+            if (knownGlyphs.Contains(hieroglyphic))
+                return;
+            knownGlyphs.Add(hieroglyphic);
+        }
+
+        public void Talk(Hierogplyph hieroglyphic)
+        {
+            latestTalk = hieroglyphic;
+            DialogueSystem.Talking(this);
+        }
+
+        public Vector2 GetLocation()
+        {
+            return transform.position;
+        }
+
+        public void OnHearing(ITalker talker)
+        {
+            LearnNewGlyph(talker.GetLatest());
+        }
+
+        public Hierogplyph GetLatest()
+        {
+            return latestTalk;
+        }
+
+        public float GetTalkRange()
+        {
+            return talkRange;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position,talkRange);
+        }
+    }
+}
