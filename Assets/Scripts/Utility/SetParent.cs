29ff0711d66d385416aaaf54cf7a4c97b85f7b17
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-01-31 10:38:37 +0100

    Hidden Borders, Game Update

--- Assets/Scripts/Utility/SetParent.cs
+++ Assets/Scripts/Utility/SetParent.cs
@@ -1,16 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Game
 {
     public class SetParent : MonoBehaviour
     {
-        private Dictionary<GameObject, Transform> prevoiusParent = new Dictionary<GameObject, Transform>();
+        private Dictionary<GameObject, StateOnEnter> prevoiusParent = new Dictionary<GameObject, StateOnEnter>();
 
         public Transform parentingObject;
 
+        class StateOnEnter
+        {
+            public Transform realParent;
+            public bool wasKinimatic;
+
+            public StateOnEnter(Transform realParent, bool wasKinimatic)
+            {
+                this.realParent = realParent;
+                this.wasKinimatic = wasKinimatic;
+            }
+        }
+        
+        public void Lock()
+        {
+            foreach (KeyValuePair<GameObject,StateOnEnter> stateOnEnter in prevoiusParent)
+            {
+                Lock(stateOnEnter.Key);
+            }
+        }
+
+        public void UnLock()
+        {
+            foreach (KeyValuePair<GameObject,StateOnEnter> stateOnEnter in prevoiusParent)
+            {
+                Unlock(stateOnEnter.Key);
+            }
+        }
+
+        private void Unlock(GameObject gameObject)
+        {
+            gameObject.transform.SetParent(prevoiusParent[gameObject].realParent);
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = prevoiusParent[gameObject].wasKinimatic;
+        }
+
+        private void Lock(GameObject gameObject)
+        {
+            gameObject.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            gameObject.transform.SetParent(parentingObject);
+        }
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player") || other.CompareTag("Monster"))
@@ -18,9 +59,9 @@ namespace Game
                 // If player or a Character Enters
                 if (prevoiusParent.ContainsKey(other.gameObject))
                     return;
-                prevoiusParent.Add(other.gameObject, other.transform.parent);
-                other.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                other.transform.SetParent(parentingObject);
+                
+                //Add to List
+                prevoiusParent.Add(other.gameObject, new StateOnEnter(other.transform.parent,other.attachedRigidbody.isKinematic));
             }
         }
         private void OnTriggerExit2D(Collider2D other)
@@ -28,8 +69,7 @@ namespace Game
             // If player or a Character Enters
             if(prevoiusParent.ContainsKey(other.gameObject) == false)
                 return;
-            other.transform.SetParent(prevoiusParent[other.gameObject]);
-            other.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            Unlock(other.gameObject);
             prevoiusParent.Remove(other.gameObject);
         }
     }
