f739a2ed7021b031a89f701af41b8505d32b1562
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-01-31 16:34:58 +0100

    GameManager Forces our Wanted Resolution

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 
 namespace Game
 {
     public class GameManager : MonoBehaviour
     {
-
-
+        public void Start()
+        {
+            // Force Resolution
+            Screen.SetResolution(960,540,FullScreenMode.Windowed);
+        }
     }
 }
