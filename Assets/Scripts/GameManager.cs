40636886b33aa6bd98e8a4d92b0241a7c1899389
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-01-31 17:59:36 +0100

    Final changes - Latest Build

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,35 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Game
 {
     public class GameManager : MonoBehaviour
     {
+        private void Awake()
+        {
+            Screen.SetResolution(960,540,FullScreenMode.Windowed);
+            BorderlessWindow.InitializeOnLoad();
+        }
+
         public void Start()
         {
             // Force Resolution
+            BorderlessWindow.SetFramelessWindow();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private void MakeFrameLess()
+        {
+            BorderlessWindow.SetFramelessWindow();
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private void SetResolution()
+        {
             Screen.SetResolution(960,540,FullScreenMode.Windowed);
         }
+        
+        
     }
 }
